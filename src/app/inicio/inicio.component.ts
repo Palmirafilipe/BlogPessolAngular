f350a274cca77dfaf0a3012f6aed4b1fb192c2a6
Palmira Filipe <palmirafilipe24@gmail.com>
2021-05-10 23:06:09 -0300

    Implemantação do post e get de postagens

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscriber } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -9,8 +16,21 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class InicioComponent implements OnInit {
 
+  postagem: Postagem = new Postagem()
+  listaPostagens: Postagem[]
+  
+  tema: Tema = new Tema()
+  listaTema: Tema[]
+  idTema: number
+
+  user: User = new User()
+  idUser = environment.id
+
   constructor(
-    private router: Router
+    private router: Router,
+    private postagemService: PostagemService,
+    private temaService: TemaService,
+    private authSevise: AuthService
   ) { }
 
   ngOnInit() {
@@ -18,6 +38,49 @@ export class InicioComponent implements OnInit {
     if(environment.token ==''){
       this.router.navigate(['/entar'])
     }
+
+    this.getAllTema()
+    this.getAllPostagens()
+  }
+
+  getAllTema(){
+    this.temaService.getAllTema().subscribe((resp: Tema[])=>{
+      this.listaTema = resp
+    })
+  }
+
+  findByIdTema(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema)=>{
+      this.tema = resp
+    })
+  }
+
+  getAllPostagens(){
+    this.postagemService.getAllPostagens().subscribe((resp: Postagem[])=>{
+      this.listaPostagens =resp
+    })
+  }
+
+  findAllByUser(){
+    this.authSevise.getByIdUser(this.idUser).subscribe((resp: User)=>{
+      this.user = resp
+    })
+  }
+
+  publicar(){
+    this.tema.id = this.idTema
+    this.postagem.tema = this.tema
+
+    this.user.id = this.idUser
+    this.postagem.usuario = this.user
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem)=>{
+      this.postagem = resp
+      alert('Postagem realizada com sucesso!')
+      this.postagem = new Postagem()
+      this.getAllPostagens()
+    })
+
   }
 
 }
